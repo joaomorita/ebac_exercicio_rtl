1910eefdcd6d7be6b1af87c9e6fb14b0bc214e98
Morita <joao@teste.com>
2025-04-16 11:09:23 -0300

    implementa teste no comentário

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,31 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve comentários', () => {
+        // Renderiza o post comment
+        render(<PostComment />)
+
+        //usa o change para alterar o text area e adicionar "Comentário 1"
+        fireEvent.change(screen.getByTestId('textarea-comment-id'), {
+            target: {
+                value: 'Comentário 1'
+            }
+        })
+        // usa o click para clicar no botão "comentar"
+        fireEvent.click(screen.getByTestId('botao-teste-id'))
+
+        // Adicionando segundo comentário
+        fireEvent.change(screen.getByTestId('textarea-comment-id'), {
+            target: {
+                value: 'Comentário 2'
+            }
+        })
+
+        // Clicando para adicionar o segundo comentário
+        fireEvent.click(screen.getByTestId('botao-teste-id'))
+
+        // Espera que exista 2 <li>
+        expect(screen.getAllByTestId('comment-test-id')).toHaveLength(2)
+    })
 });
